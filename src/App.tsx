c48f75ef9db953bfdefb5e30f5a52b3fb2cb3446
Giridhar Nair <Giridhar.r.nair@gmail.com>
2026-06-03 16:45:08 -0500

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import { Button } from "#components/ui/button";
+import {
+    Field,
+    FieldGroup,
+    FieldLegend,
+    FieldSeparator,
+    FieldSet,
+} from "#components/ui/field";
+import { Input } from "#components/ui/input";
 
 function App() {
     return (
-        <Button variant="outline" size="lg">
-            Hello World
-        </Button>
+        <div className="flex h-screen items-center justify-center w-full">
+            <form className="w-90">
+                <FieldGroup>
+                    <FieldSet>
+                        <FieldLegend>Join Room</FieldLegend>
+                        <FieldGroup>
+                            <Field orientation="vertical">
+                                <Input
+                                    id="room-code-input"
+                                    placeholder="Enter code"
+                                    required
+                                />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+
+                    <FieldSeparator />
+
+                    <FieldSet>
+                        <FieldGroup>
+                            <Button type="button" className="w-full">
+                                Create Room
+                            </Button>
+                        </FieldGroup>
+                    </FieldSet>
+                </FieldGroup>
+            </form>
+        </div>
     );
 }
 
