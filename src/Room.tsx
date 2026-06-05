fc485eb6250454a816024bf221c3180097e8f169
Giridhar Nair <Giridhar.r.nair@gmail.com>
2026-06-05 12:30:28 -0500

    Integrated Supabase collaborative code editor

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,8 +1,19 @@
 import { useParams } from "react-router";
+import { RealtimeMonaco } from "#components/realtime-monaco";
 
 function Room() {
     const { roomId } = useParams();
-    return <div>Room: {roomId}</div>;
+
+    if (!roomId) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h1>Room: {roomId}</h1>
+            <RealtimeMonaco channel={roomId} height="90vh" />
+        </div>
+    );
 }
 
 export default Room;
