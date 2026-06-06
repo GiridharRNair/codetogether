abfb550c785c82c6be234ef738d34beb92c32397
Giridhar Nair <Giridhar.r.nair@gmail.com>
2026-06-06 18:54:52 -0500

    Attempt to deploy python api to Vercel

--- api/main.py
+++ api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 
-@app.get("/")
+@app.get("/api/health")
 def read_root():
-    return {"Hello": "World"}
+    return {"status": "healthy"}
